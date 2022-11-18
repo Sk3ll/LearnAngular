ee160d43265b3a36ce653928acaed4dda9703e2c
Yaroslav Malykhin <sk3llyar@gmail.com>
2022-11-18 12:00:49 +0200

    feat(details): Separated details to another component, used Input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
